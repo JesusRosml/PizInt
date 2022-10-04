aa4cbb17aef2c6a20c8b5645d14fa40fcd766da6
JesusRosml <105245158+JesusRosml@users.noreply.github.com>
2022-10-03 23:34:04 -0500

    pantallas enlazadas creadas

---
+++ app/src/main/java/Frontend_Pantallas/Informacion_esc.java
@@ -0,0 +1,16 @@
+package Frontend_Pantallas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jesus.pizint.R;
+
+public class Informacion_esc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_informacion_esc);
+    }
+}
---
+++ app/src/main/java/Frontend_Pantallas/pantallaextra.java
@@ -0,0 +1,16 @@
+package Frontend_Pantallas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jesus.pizint.R;
+
+public class pantallaextra extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantallaextra);
+    }
+}
