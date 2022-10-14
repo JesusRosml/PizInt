36d6434ac0ce0b3b6620e6de5b4bcdc0e0b56506
Angel Ricardo Miss Blanco <angelitomiss@yahoo.com.mx>
2022-10-13 21:18:16 -0500

    Seccion musica hecha

--- app/src/main/java/com/jesus/pizint/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/jesus/pizint/ui/slideshow/SlideshowFragment.java
@@ -1,18 +1,27 @@
 package com.jesus.pizint.ui.slideshow;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.jesus.pizint.R;
 import com.jesus.pizint.databinding.FragmentSlideshowBinding;
 
+import Frontend_Pantallas.Exclusive_Login;
+
+
 public class SlideshowFragment extends Fragment {
+    Button PagInicio;
 
     private FragmentSlideshowBinding binding;
 
@@ -22,11 +31,24 @@ public class SlideshowFragment extends Fragment {
                 new ViewModelProvider(this).get(SlideshowViewModel.class);
 
         binding = FragmentSlideshowBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+            // Root = view NOT View
+            View root = binding.getRoot();
 
         final TextView textView = binding.textSlideshow;
         slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        PagInicio = (Button) root.findViewById(R.id.button_pag_iniciar);
+        PagInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent siguiente_pag = new Intent(getActivity(), Exclusive_Login.class);
+                startActivity(siguiente_pag);
+            }
+        });
         return root;
+
+
+
     }
 
     @Override
