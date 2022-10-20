b08455b37f407e69cad905d8075edfcab307a1d1
JesusRosml <105245158+JesusRosml@users.noreply.github.com>
2022-10-20 16:33:08 -0500

    botos flotantes

--- app/src/main/java/Frontend_Pantallas/SplashScreen.java
+++ app/src/main/java/Frontend_Pantallas/SplashScreen.java
@@ -7,9 +7,7 @@ import android.os.Bundle;
 import android.os.Handler;
 
 import com.jesus.pizint.BarraNavegacionDesplegable;
-import com.jesus.pizint.MainActivity;
 import com.jesus.pizint.R;
-import com.jesus.pizint.ui.home.HomeFragment;
 
 public class SplashScreen extends AppCompatActivity {
 
--- app/src/main/java/com/jesus/pizint/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/jesus/pizint/ui/slideshow/SlideshowFragment.java
@@ -1,10 +1,13 @@
 package com.jesus.pizint.ui.slideshow;
 
+import static androidx.databinding.DataBindingUtil.setContentView;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,9 +17,9 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.jesus.pizint.R;
 import com.jesus.pizint.databinding.FragmentSlideshowBinding;
-
 import Frontend_Pantallas.Exclusive_Login;
 
 
