54914ed2d681ce8b988112f1d18febafa93c3a07
JesusRosml <105245158+JesusRosml@users.noreply.github.com>
2022-11-11 06:01:50 -0600

    Seccion/PublicacionesFuncional4.0

---
+++ app/src/main/java/com/jesus/pizint/PruebasBaseDatos.java
@@ -0,0 +1,14 @@
+package com.jesus.pizint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PruebasBaseDatos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pruebas_base_datos);
+    }
+}
