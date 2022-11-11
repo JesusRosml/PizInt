d295bbab72ca411451d10989eb3b288a3ff4d7c7
JesusRosml <105245158+JesusRosml@users.noreply.github.com>
2022-11-10 21:30:11 -0600

    Seccion/PublicacionesFuncional

---
+++ app/src/main/java/Frontend_Pantallas/Public_NoticeOff.java
@@ -0,0 +1,62 @@
+package Frontend_Pantallas;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.jesus.pizint.R;
+
+public class Public_NoticeOff extends AppCompatActivity {
+
+    ImageView imageView;
+    ImageButton btn;
+
+    private static final int SELECT_IMAGE = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_public_notice_off);
+
+        btn = findViewById(R.id.pickImage);
+        imageView = findViewById(R.id.image);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(
+                        Intent.createChooser(intent,"Select Image"),
+                        SELECT_IMAGE
+                );
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode,
+                                    int resultCode,
+                                    @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == SELECT_IMAGE && null != data)
+        {
+            Uri uri = data.getData();
+            imageView.setImageURI(uri);
+        }
+    }
+
+}
--- app/src/main/java/com/jesus/pizint/ui/home/HomeFragment.java
+++ app/src/main/java/com/jesus/pizint/ui/home/HomeFragment.java
@@ -1,18 +1,24 @@
 package com.jesus.pizint.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.jesus.pizint.R;
 import com.jesus.pizint.databinding.FragmentHomeBinding;
 
+import Frontend_Pantallas.Public_NoticeOff;
+
 public class HomeFragment extends Fragment {
+    Button pag;
 
     private FragmentHomeBinding binding;
 
@@ -26,12 +32,25 @@ public class HomeFragment extends Fragment {
 
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        pag = (Button) root.findViewById(R.id.publicnotice1);
+        pag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent siguiente_pag = new Intent(getActivity(), Public_NoticeOff
+                        .class);
+                startActivity(siguiente_pag);
+            }
+        });
         return root;
     }
+
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
+
+
 }
